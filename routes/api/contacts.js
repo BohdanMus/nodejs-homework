024422c1247b7550c87b0faff635befb56be1d10
BohdanMus <112251293+BohdanMus@users.noreply.github.com>
2023-07-07 03:47:56 +0300

    finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody } = require("../../middlewares");
-const schemas = require("../../schemas/contacts");
+const { validateBody, isValidId } = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+
+const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+router.get("/:id", isValidId, ctrl.getById);
 
-router.post("/", validateBody(schemas), ctrl.add);
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id", validateBody(schemas), ctrl.updateById);
+router.put("/:id", validateBody(schemas.addSchema), isValidId, ctrl.updateById);
 
-router.delete("/:id", ctrl.deleteById);
+router.patch("/:id/favorite", isValidId, ctrl.updateStatusContact);
+
+router.delete("/:id", isValidId, ctrl.deleteById);
 
 module.exports = router;
