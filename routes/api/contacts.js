04dbba16e42f6564ec45f1e1db35d1a56fd171cb
BohdanMus <112251293+BohdanMus@users.noreply.github.com>
2023-07-14 23:12:31 +0300

    finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
-const { validateBody, isValidId } = require("../../middlewares");
+const { validateBody, isValidId, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/contact");
 const { ctrlWrapper } = require("../../utils");
 
-router.get("/", ctrlWrapper(ctrl.getAllContacts));
+router.get("/", authenticate, ctrlWrapper(ctrl.getAllContacts));
 
-router.get("/:id", isValidId, ctrlWrapper(ctrl.getContactById));
+router.get("/:id", authenticate, isValidId, ctrlWrapper(ctrl.getContactById));
 
-router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.addContact));
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrlWrapper(ctrl.addContact)
+);
 
 router.put(
   "/:id",
+  authenticate,
   validateBody(schemas.addSchema),
   isValidId,
   ctrlWrapper(ctrl.updateContactById)
 );
 
-router.patch("/:id/favorite", isValidId, ctrlWrapper(ctrl.updateStatusContact));
+router.patch(
+  "/:id/favorite",
+  authenticate,
+  isValidId,
+  ctrlWrapper(ctrl.updateStatusContact)
+);
 
-router.delete("/:id", isValidId, ctrlWrapper(ctrl.deleteContactBiId));
+router.delete(
+  "/:id",
+  authenticate,
+  isValidId,
+  ctrlWrapper(ctrl.deleteContactBiId)
+);
 
 module.exports = router;
